740aef9d55b203a8e547b7e8c41021a05b86ec79
courtney persons <personscourtney@gmail.com>
2018-09-19 14:44:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
 if (number % 3 == 0) && (number % 5 == 0)
-  "fizzbuzz"
+  "FizzBuzz"
 elsif number % 3 == 0 # if the number int is divisible by 3
    "Fizz"
  elsif number % 5 == 0
