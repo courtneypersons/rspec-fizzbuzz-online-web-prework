b42a66e70416e202336a3ee1bd5de216a8cd766c
courtney persons <personscourtney@gmail.com>
2018-09-19 14:42:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-elsif (number % 3 == 0) && (number % 5 == 0)
+if (number % 3 == 0) && (number % 5 == 0)
   "fizzbuzz"
-  if number % 3 == 0 # if the number int is divisible by 3
+elsif number % 3 == 0 # if the number int is divisible by 3
    "Fizz"
  elsif number % 5 == 0
    "Buzz" # Go fizz
